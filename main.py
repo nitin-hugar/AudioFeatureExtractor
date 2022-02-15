565ab6dc986c4627d97466795822162fe9e8d86f
nitin-hugar <nhugar3@gatech.edu>
2022-02-14 20:49:02 -0500

    Add comments and fix rms zero error

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 
+"""
+Script to extract multiple features from audio and save them to a file
+
+Author: Nitin Hugar
+"""
+
+#===============================================================================
+# Import dependancies
+#===============================================================================
+
 import madmom
 import librosa
 import numpy as np
@@ -13,13 +23,19 @@ import msaf
 import math
 import time
 
+#===============================================================================
+# Function Definitions 
+#===============================================================================
 
+
+# Function imports audio from a given audio file path
 def import_audio(audioFilePath):
     data, samplingrate = sf.read(audioFilePath)
     if data.shape[1] == 2:
         data = data[:, 0]
     return data, samplingrate
 
+# Function blocks audio into chunks
 def block_audio(x, blockSize, hopSize, fs):
     # allocate memory
     numBlocks = math.ceil(x.size / hopSize)
@@ -34,6 +50,7 @@ def block_audio(x, blockSize, hopSize, fs):
         xb[n][np.arange(0, blockSize)] = x[np.arange(i_start, i_stop + 1)]
     return xb, t
 
+# Function computes a fourier transform of an audio block and returns the magnitude spectrum
 def fourier(x):
     # Get Symmetric fft
     w = signal.windows.hann(np.size(x))
@@ -49,6 +66,7 @@ def fourier(x):
     magX = abs(X[0:int(x.size // 2 + 1)])
     return magX
 
+# Function extracts feature "Spectral crest" of audio block
 def extract_spectral_crest(xb):
     magX = np.zeros((xb.shape[0], int(xb.shape[1] / 2 + 1)))
     spc = np.zeros(xb.shape[0])
@@ -60,6 +78,7 @@ def extract_spectral_crest(xb):
         spc[block] = np.max(magX[block]) / summa
     return spc
 
+# Function extracts feature "Spectral flux" of audio block
 def extract_spectral_flux(xb):
     magX = np.zeros((xb.shape[0], int(xb.shape[1] / 2 + 1)))
     specflux = np.zeros((xb.shape[0]))
@@ -70,41 +89,33 @@ def extract_spectral_flux(xb):
         specflux[block] = np.sqrt(np.sum(np.square(magX[block] - magX[block - 1]))) / den
     return specflux
 
-
+# Function returns onset times in an audio file 
 def detect_onsets(audioFilePath):
     proc = madmom.features.onsets.OnsetPeakPickingProcessor(fps=100)
     act = madmom.features.onsets.RNNOnsetProcessor()(audioFilePath)
     onsets = proc(act)
     return onsets
 
+# Function returns beat times in an audio file
 def detect_beats(audioFilePath):
     b = madmom.features.beats.RNNBeatProcessor()(audioFilePath)
     beats = madmom.features.beats.BeatTrackingProcessor(fps=100)(b)
     return beats
 
+# Function returns downbeats in an audio file
 def detect_downbeats(audioFilePath):
     proc = madmom.features.downbeats.DBNDownBeatTrackingProcessor(beats_per_bar=4, min_bpm=60, max_bpm=160, correct=True, fps=100)  # check
     act = madmom.features.downbeats.RNNDownBeatProcessor()(audioFilePath)
     downbeats = proc(act)
     return downbeats
 
-# def detect_tempo(audioFilePath):
-#     beats = madmom.features.beats.RNNBeatProcessor()(audioFilePath)
-#     when_beats = madmom.features.beats.BeatTrackingProcessor(fps=100)(beats)
-#     m_res = scipy.stats.linregress(np.arange(len(when_beats)), when_beats)
-
-#     first_beat = m_res.intercept
-#     beat_step = m_res.slope
-#     detected_tempo = 60 / beat_step
-#     if detected_tempo > 160:
-#         detected_tempo = detected_tempo // 2
-#     return int(np.ceil(detected_tempo))
-
+# Function returns tempo of an audio file
 def detect_tempo(audioFilePath):
     x, fs = import_audio(audioFilePath)
     tempo, beats = librosa.beat.beat_track(x, sr=fs, units='time') # 'units : 'time', 'frames', 'samples'
     return tempo 
     
+# Function returns key of an audio file
 def detect_key(audioFilePath):
     key_probabilities = madmom.features.key.CNNKeyRecognitionProcessor()
     key_prediction = madmom.features.key.key_prediction_to_label(key_probabilities(audioFilePath))
@@ -126,12 +137,16 @@ def detect_chords_CRF(audioFilePath):
     chords = chordrec(audioFilePath)
     return chords
 
+# Function extracts rms feature from audio blocks 
 def extract_rms(data, blockSize, hopSize, fs):
     xb, t = block_audio(data, blockSize, hopSize, fs)
     rms = np.zeros(xb.shape[0])
     for block in range(xb.shape[0]):
-        rms[block] = np.sqrt(np.mean(np.square(xb[block])))
-        rms[block] = 20 * np.log10(rms[block])
+        if not np.all((xb[block] == 0)): # Check for zero errors
+            rms[block] = np.sqrt(np.mean(np.square(xb[block])))
+            rms[block] = 20 * np.log10(rms[block])
+        else: 
+            rms[block] = 0
     return rms
 
 # creates a voicing mask which is 1 when the audio level is above or equal to threhold and 0 when below
@@ -145,15 +160,19 @@ def detect_silence(data, blockSize, hopSize, fs, thresholdDb):
             mask[i] = 0
     return mask
 
+
+# Function extracts section boundaries and labels of an audio file
 def extract_sections(audioFilePath):
     boundaries, labels = msaf.process(audioFilePath, sonify_bounds=False,
                                         boundaries_id='foote', labels_id="cnmf")
     return boundaries, labels
 
+# Helper function converts frequency to midi
 def freq2midi(freq):
     midi = 69 + 12 * np.log2(freq / 440)
     return np.asarray(midi, dtype=np.int32)
 
+# Helper function comverts midi values to notes
 def midi_to_notes(midi, fs, hopSize, smooth, minduration):
     # smooth midi pitch sequence first
     if smooth > 0:
@@ -216,9 +235,13 @@ def pitch_to_midi(data, fs, hopSize):
     notes = midi_to_notes(midiNotes, fs, hopSize=hopSize, smooth=0.40, minduration=0.1)
     return notes
 
+#===============================================================================
+# Main function
+#===============================================================================
 
 def main():
    
+    # Comment out any feature that you do not want to extract
 
     audioFilePath = input("Enter audio filepath: ")
 
@@ -226,48 +249,48 @@ def main():
     print("Importing file...")
     data, fs = import_audio(audioFilePath)
 
-    # print("Extracting onsets...")
-    # onsets = detect_onsets(audioFilePath)
+    print("Extracting onsets...")
+    onsets = detect_onsets(audioFilePath)
 
-    # print("Extracting Beats...")
-    # beats = detect_beats(audioFilePath)
+    print("Extracting Beats...")
+    beats = detect_beats(audioFilePath)
 
-    # print("Extracting Downbeats...")
-    # downbeats = detect_downbeats(audioFilePath)
+    print("Extracting Downbeats...")
+    downbeats = detect_downbeats(audioFilePath)
 
     print("Extracting Tempo...")
     tempo = detect_tempo(audioFilePath)
 
-    # print("Extracting Key...")
-    # key = detect_key(audioFilePath)
+    print("Extracting Key...")
+    key = detect_key(audioFilePath)
 
-    # print("Extracting Chords...")
-    # chords = detect_chords_deep_chroma(audioFilePath)
+    print("Extracting Chords...")
+    chords = detect_chords_deep_chroma(audioFilePath)
 
-    # print("Extracting RMS...")
-    # rms = extract_rms(data, 1024, 512, fs)
+    print("Extracting RMS...")
+    rms = extract_rms(data, 1024, 512, fs)
 
-    # print("Extracting Silence...")
-    # silence = detect_silence(data, 1024, 512, fs, thresholdDb=-40)
+    print("Extracting Silence...")
+    silence = detect_silence(data, 1024, 512, fs, thresholdDb=-40)
 
-    # print("Extracting Boundaries...")
-    # boundaries, _ = extract_sections(audioFilePath)
-    # notes = pitch_to_midi(data, fs, hopSize=512)
-    # print(notes)
+    print("Extracting Boundaries...")
+    boundaries, _ = extract_sections(audioFilePath)
+    notes = pitch_to_midi(data, fs, hopSize=512)
+    print(notes)
     end = time.time()
     print(end-start)
     print("Done!")
     
     feature_dict = {}
-    # feature_dict['onsets'] = onsets
-    # feature_dict['beats'] = beats
-    # feature_dict['downbeats'] = downbeats
+    feature_dict['onsets'] = onsets
+    feature_dict['beats'] = beats
+    feature_dict['downbeats'] = downbeats
     feature_dict['tempo'] = tempo
-    # feature_dict['key'] = key
-    # feature_dict['chords'] = chords
-    # feature_dict['rms'] = rms
-    # feature_dict['silence'] = silence
-    # feature_dict['boundaries'] = boundaries
+    feature_dict['key'] = key
+    feature_dict['chords'] = chords
+    feature_dict['rms'] = rms
+    feature_dict['silence'] = silence
+    feature_dict['boundaries'] = boundaries
     
     with open('feature_dict.csv', 'w') as f:
         for key in feature_dict.keys():
@@ -277,14 +300,6 @@ def main():
 if __name__ == '__main__': 
     main()
 
-# user_input = input("Split stems (y/n)?")
-# if user_input == 'y':
-#     os.system("conda activate splitter2")
-#     os.system("spleeter separate -p spleeter:5stems -o output ../../JustGotPaid.wav")
-#     os.system("conda deactivate")
-#     os.system("source bin/activate")
-# elif user_input == 'n':
-#     main()
-#
-# else:
-#     print("Enter y or n")
+
+
+
